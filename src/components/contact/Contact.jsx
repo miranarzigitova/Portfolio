4eabc3e6ebc326526b156a78f6369cbb035dc8ca
miranarzhigitova <miranaa0000@gmail.com>
2023-09-11 14:46:36 +0600

    .

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,29 +1,24 @@
 import React from 'react'
 import './contact.css'
-import { useRef } from 'react'
-import { useState } from 'react'
-import emailjs from 'emailjs-com'
 import {BsEnvelope} from 'react-icons/bs'
-import {FaHatWizard} from 'react-icons/fa'
+import {FaGithubSquare} from 'react-icons/fa'
 import {BsWhatsapp} from 'react-icons/bs'
+import {FaTelegramPlane} from 'react-icons/fa'
 
 
 const Contact = () => {
-  const form = useRef();
-  const [msg, setMsg] = useState("");
-  
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm('service_k21tg8s', 'template_8aq38hi', form.current, '5SzJoWUkRHKf1tpY1')
-    setMsg("Message")
-    e.target.reset()
-  };
+ 
   return (
     <section id='contact'>
       <h2 className='text-light'>Contacts</h2>
       <div className="container contact__container">
         <div className="contact__options">
+        <article className="contact__option">
+            <FaTelegramPlane className="contact__option-icon"/>
+            <h4>Telegram</h4>
+            <h5>@qwertytest</h5>
+            <a href="#" target="_blank" rel="noopener noreferrer">Message</a>
+          </article>
           <article className="contact__option">
             <BsEnvelope className="contact__option-icon"/>
             <h4>E-mail</h4>
@@ -31,8 +26,8 @@ const Contact = () => {
             <a href="#">Message</a>
           </article>
           <article className="contact__option">
-            <FaHatWizard className="contact__option-icon"/>
-            <h4>Site</h4>
+            <FaGithubSquare className="contact__option-icon"/>
+            <h4>Portfolio GitHub</h4>
             <h5>GitHub</h5>
             <a href="#" target="_blank" rel="noopener noreferrer">git.com</a>
           </article>
@@ -43,12 +38,6 @@ const Contact = () => {
             <a href="#" target="_blank" rel="noopener noreferrer">Message</a>
           </article>
         </div>
-          <form ref={form} onSubmit={sendEmail}>
-            <input type="text" name="name" placeholder="Name" required />
-            <input type="email" name="email" placeholder="Email" required />
-            <textarea name="message" rows="7" required placeholder="Text"></textarea>
-            <button type="submit" className="btn btn-primary">Send</button><span>{msg}</span>
-          </form>
       </div>
     </section>
   )
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,22 +1,60 @@
-import React from 'react'
-import './nav.css'
-import {AiFillHome} from 'react-icons/ai'
-import {FaUserCircle} from 'react-icons/fa'
-import {SiPaloaltonetworks} from 'react-icons/si'
-import {MdComputer} from 'react-icons/md'
-import {MdMessage} from 'react-icons/md'
-import {useState} from 'react'
+import React from "react";
+import "./nav.css";
+import { AiFillHome } from "react-icons/ai";
+import { FaUserCircle } from "react-icons/fa";
+import { SiPaloaltonetworks } from "react-icons/si";
+import { MdComputer } from "react-icons/md";
+import { MdMessage } from "react-icons/md";
+import { AiFillFolderOpen } from "react-icons/ai";
+import { useState } from "react";
 const Nav = () => {
-  const[activeNav, setActiveNav] = useState('#')
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <nav>
-    <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : '' }><AiFillHome /></a>
-    <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : '' }><FaUserCircle /></a>
-    <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : '' }><SiPaloaltonetworks /></a>
-    <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : '' }><MdComputer /></a>
-    <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : '' }><MdMessage /></a>
-  </nav>
-  )
-}
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <AiFillHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <FaUserCircle />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <SiPaloaltonetworks />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        <MdComputer />
+      </a>
+      <a
+        href="#portfolio"
+        onClick={() => setActiveNav("#portfolio")}
+        className={activeNav === "#portfolio" ? "active" : ""}
+      >
+        <AiFillFolderOpen />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <MdMessage />
+      </a>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
